a8c902aee4fef50d6398598910d9a69e39a1982f
guedetodd <guedetodd@gmail.com>
2025-07-11 07:32:21 -0500

    refactor code to be cleaner

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-from stats import word_counter
+from stats import (
+    full_book,
+    word_counter,
+    letter_counter,
+    formatted
+        )
+import sys
 
-def main(book_path = "books/frankenstein.txt"):
-    text = get_book_text(book_path)
-    num_words = word_counter(text)
-    print(f"{num_words} words found in the document")
-
-def get_book_text(book):
-    with open(book) as f:
-        return f.read()
+def main(book_path):
+    book_contents = full_book(book_path)
+    word_count = word_counter(book_contents)
+    l_count = letter_counter(book_contents)
+    formatted(word_count, l_count)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,30 @@
+def full_book(book_path):
+    with open(book_path) as f:
+        return f.read()
+
 def word_counter(text):
     words = text.split()
     return len(words)
+
+def letter_counter(text):
+    lower = text.lower()
+    l_count = {}
+
+    for letter in lower:
+        if letter in l_count:
+            l_count[letter]["num"] +=1
+        else:
+            l_count[letter] = {"char": letter, "num": 1}
+    sorted_l_count = list(sorted(l_count.values(), key=lambda item: item["num"], reverse=True))
+    return sorted_l_count
+
+def formatted(word_count, l_count):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for letter in l_count:
+        if letter["char"].isalpha():
+            print(f"{letter['char']}: {letter['num']}")
+    print("============= END ===============")
