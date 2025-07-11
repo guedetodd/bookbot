3d30c8c5fd3c3e28bb6ddbf51d771b3de0cf720c
guedetodd <guedetodd@gmail.com>
2025-07-11 08:13:40 -0500

    final commit

--- main.py
+++ main.py
@@ -6,10 +6,15 @@ from stats import (
         )
 import sys
 
-def main(book_path):
-    book_contents = full_book(book_path)
-    word_count = word_counter(book_contents)
-    l_count = letter_counter(book_contents)
-    formatted(word_count, l_count)
+args = sys.argv
 
-main()
+def main(arguments):
+    if len(arguments) == 2:
+        book_contents = full_book(arguments[1])
+        word_count = word_counter(book_contents)
+        l_count = letter_counter(book_contents)
+        formatted(word_count, l_count)
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+main(args)
